9521b20a3fc38ffb50f1d22cae0b935e31560967
faithchan <fch@mercantilemaritime.com>
2021-12-21 01:50:30 +1100

    About Page Done

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,39 +1,13 @@
 import React from 'react'
 
 import AboutSection from './AboutSection'
-import GP from "../service-10.png"
-import Wellbeing from "../service-11.png"
-import Paediatrics from "../service-12.png"
+import ServicesSection from './ServicesSection'
 
 const HomePage = () => {
     return (
         <div >
            <AboutSection />
-
-           <div className="mt-10">
-           <h1 className="text-center font-MT font-semibold text-3xl text-blue-400">
-               About Us
-           </h1>
-           <p className="text-center font-MT text-sm mt-6 ">BlueMed was created to transform traditional healthcare, with BlueMed, patients can <br/> now have access to doctors in a remote setting at anytime.</p>
-           <div className="grid grid-cols-1 mx-40 mt-8 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-            <span className=" px-20 mb-10 h-auto w-200px">
-        <img src={GP} alt="GP" className=""/> 
-        <p className="font-semibold font-MT text-blue-400 mt-4 text-xl text-center">General<br/>Practitioner</p>
-        </span>
-
-        <span className="w-full px-20 mb-10 ">
-        <img src={Wellbeing} alt="Wellbeing" className="block h-auto w-200px"/> 
-        <p className="font-semibold font-MT text-blue-400 mt-4 text-xl text-center">Mental<br/>Well-being</p>
-        </span>
-
-        <span className="w-full px-20 mb-10 ">
-        <img src={Paediatrics} alt="Paediatrics" className="block h-auto w-200px"/> 
-        <p className="font-semibold font-MT text-blue-400 mt-4 text-xl text-center">Paediatrics<br/>(Infant Care)</p>
-        </span>
-
-            </div>
-    )
-           </div>
+            <ServicesSection />
 
            
         </div>
--- src/components/ServicesSection.js
+++ src/components/ServicesSection.js
@@ -5,20 +5,30 @@ import Paediatrics from "../service-12.png"
 
 const ServicesSection = () => {
     return (
-        <div className="grid grid-cols-1 mx-28 my-8 sm:grid-cols-2 md:grid-cols-3">
-            <span className="w-full px-5 mb-10">
-        <img src={GP} alt="GP" className=""/> 
-        </span>
+        <div className="mt-10">
+        <h1 className="text-center font-MT font-semibold text-3xl text-blue-400">
+            About Us
+        </h1>
+        <p className="text-center font-MT text-sm mt-6 ">BlueMed was created to transform traditional healthcare, with BlueMed, patients can <br/> now have access to doctors in a remote setting at anytime.</p>
+        <div className="grid grid-cols-1 mx-40 mt-8 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+         <span className=" px-20 mb-10 h-auto w-200px">
+     <img src={GP} alt="GP" className=""/> 
+     <p className="font-semibold font-MT text-blue-400 mt-4 text-xl text-center">General<br/>Practitioner</p>
+     </span>
 
-        <span className="w-full px-5 mb-10 ">
-        <img src={Wellbeing} alt="Wellbeing" className="block h-auto w-full"/> 
-        </span>
+     <span className="w-full px-20 mb-10 ">
+     <img src={Wellbeing} alt="Wellbeing" className="block h-auto w-200px"/> 
+     <p className="font-semibold font-MT text-blue-400 mt-4 text-xl text-center">Mental<br/>Well-being</p>
+     </span>
 
-        <span className="w-full px-5 mb-10 ">
-        <img src={Paediatrics} alt="Paediatrics" className="block h-auto w-full"/> 
-        </span>
+     <span className="w-full px-20 mb-10 ">
+     <img src={Paediatrics} alt="Paediatrics" className="block h-auto w-200px"/> 
+     <p className="font-semibold font-MT text-blue-400 mt-4 text-xl text-center">Paediatrics<br/>(Infant Care)</p>
+     </span>
 
-            </div>
+         </div>
+ 
+        </div>
     )
 }
 
