d5d00af97c4e9b9790a65a395f8e77202c4292e8
faithchan <fch@mercantilemaritime.com>
2022-01-06 18:59:41 +0800

    NRIC field added

--- src/components/CreateAccCard.js
+++ src/components/CreateAccCard.js
@@ -8,22 +8,28 @@ const CreateAccCard = () => {
 <span className="flex justify-between">
     <span className="mr-4">
     <label className="block font-MT text-sm text-gray-700">Full name as per NRIC</label>
-    <input type="text" name="" id="" placeholder="" className="w-full px-4 py-3 rounded-lg bg-gray-200 mt-2 border focus:border-blue-500 focus:bg-white focus:outline-none" autofocus autocomplete required />
+    <input type="text" name="" id="" placeholder="" className="w-full px-4 py-2 rounded-lg bg-gray-200 mt-2 border focus:border-blue-500 focus:bg-white focus:outline-none" autofocus autocomplete required />
     </span>
   
     <span className="">
-    <label className="block font-MT text-sm text-gray-700">Email</label>
-    <input type="email" name="" id="" placeholder="" className="w-full px-4 py-3 rounded-lg bg-gray-200 mt-2 border focus:border-blue-500 focus:bg-white focus:outline-none" autofocus autocomplete required />
+    <label className="block font-MT text-sm text-gray-700">NRIC</label>
+    <input type="text" name="" id="" placeholder="" className="w-full px-4 py-2 rounded-lg bg-gray-200 mt-2 border focus:border-blue-500 focus:bg-white focus:outline-none" autofocus autocomplete required />
     </span>
+
+</span>
+<label className="block mt-2 font-MT text-sm text-gray-700">Email</label>
+    <input type="email" name="" id="" placeholder="" className="w-full px-4 py-2 rounded-lg bg-gray-200 mt-2 border focus:border-blue-500 focus:bg-white focus:outline-none" autofocus autocomplete required />
+
+<span>
 
 </span>
 
 <span className=''>
   <label className="block text-sm text-gray-700 mt-4">Password:</label>
   <span className="flex font-MT">
-  <input type="password" name="" id="" placeholder="" minlength="6" className="mr-2 w-full px-4 py-3 rounded-lg bg-gray-200 mt-2 border focus:border-blue-500
+  <input type="password" name="" id="" placeholder="" minlength="6" className="mr-2 w-full px-4 py-2 rounded-lg bg-gray-200 mt-2 border focus:border-blue-500
         focus:bg-white focus:outline-none" required />
-        <input type="password" name="" id="" placeholder="Retype password" minlength="6" className=" ml-2 w-full px-4 py-3 rounded-lg bg-gray-200 mt-2 border focus:border-blue-500
+        <input type="password" name="" id="" placeholder="Retype password" minlength="6" className=" ml-2 w-full px-4 py-2 rounded-lg bg-gray-200 mt-2 border focus:border-blue-500
         focus:bg-white focus:outline-none" required />
         </span>
 </span>
@@ -62,20 +68,20 @@ const CreateAccCard = () => {
  </span>
 
  <span className="">
-    <label className="block font-MT text-sm text-gray-700 mt-4">Address <p className="text-xs font-MT text-gray-500">Please include postal code</p></label>
-    <input type="text" name="address" id="address" placeholder="Street 23, Burlington Road, #01-174, S410897" className="w-full px-4 py-3 rounded-lg bg-gray-200 mt-2 border focus:border-blue-500 focus:bg-white focus:outline-none" autofocus autocomplete required />
+    <label className="block font-MT text-sm text-gray-700 mt-2">Address <p className="text-xs font-MT text-gray-500">Please include postal code</p></label>
+    <input type="text" name="address" id="address" placeholder="Street 23, Burlington Road, #01-174, S410897" className="w-full px-4 py-2 rounded-lg bg-gray-200 mt-2 border focus:border-blue-500 focus:bg-white focus:outline-none" autofocus autocomplete required />
     </span>
 
 
  <span className="flex justify-between mt-4">
     <span className="mr-4">
     <label className="block font-MT text-sm text-gray-700">Drug Allergies</label>
-    <input type="text" name="" id="" placeholder="" className="w-full px-4 py-3 rounded-lg bg-gray-200 mt-2 border focus:border-blue-500 focus:bg-white focus:outline-none" autofocus autocomplete required />
+    <input type="text" name="" id="" placeholder="" className="w-full px-4 py-2 rounded-lg bg-gray-200 mt-2 border focus:border-blue-500 focus:bg-white focus:outline-none" autofocus autocomplete required />
     </span>
   
     <span className="">
     <label className="block font-MT text-sm text-gray-700">Insurance ID</label>
-    <input type="email" name="" id="" placeholder="" className="w-full px-4 py-3 rounded-lg bg-gray-200 mt-2 border focus:border-blue-500 focus:bg-white focus:outline-none" autofocus autocomplete  />
+    <input type="email" name="" id="" placeholder="" className="w-full px-4 py-2 rounded-lg bg-gray-200 mt-2 border focus:border-blue-500 focus:bg-white focus:outline-none" autofocus autocomplete  />
     </span>
 
 </span>
