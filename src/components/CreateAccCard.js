6097285a53910d9fcfdda27dc7422b0a7e2decb7
andrewfaulkner <andrewfau1kn3r@gmail.com>
2022-01-07 20:27:15 +0800

    some cleanup to form

--- src/components/CreateAccCard.js
+++ src/components/CreateAccCard.js
@@ -7,13 +7,13 @@ const CreateAccCard = () => {
             case "FULLNAME":
                 return {
                     ...state,
-                    fullName: action.value,
+                    name: action.value,
 
                 }
             case "NRIC":
                 return {
                     ...state,
-                    nric: action.value,
+                    NRIC: action.value,
 
                 }
             case "EMAIL":
@@ -37,7 +37,7 @@ const CreateAccCard = () => {
             case "DOB":
                 return {
                     ...state,
-                    dob: action.value,
+                    DOB: action.value,
 
                 }
             case "GENDER":
@@ -58,6 +58,12 @@ const CreateAccCard = () => {
                     allergies: action.value,
 
                 }
+                 case "CONTACTNUMBER":
+                return {
+                    ...state,
+                    contactNumber: action.value,
+
+                }
             case "INSURANCEID":
                 return {
                     ...state,
@@ -70,14 +76,15 @@ const CreateAccCard = () => {
     }
 
     const [entry, dispatchItem] = useReducer(itemReducer, {
-        fullName: "",
-        nric: "",
+        name: "",
+        NRIC: "",
         email: "",
         password: "",
         password2: "",
-        dob: "",
+        DOB: "",
         gender: "",
         address: "",
+        contactNumber: "",
         allergies: "",
         insuranceID: ""
     })
@@ -114,6 +121,10 @@ const CreateAccCard = () => {
         
         dispatchItem({ type: "ADDRESS", value: event.target.value}) 
     }
+    const handleContactNumber = (event) => {
+        
+        dispatchItem({ type: "CONTACTNUMBER", value: event.target.value}) 
+    }
     const handleAllergies = (event) => {
         
         dispatchItem({ type: "ALLERGIES", value: event.target.value}) 
@@ -124,9 +135,13 @@ const CreateAccCard = () => {
     }
     const handleSubmit = (event) => {
         console.log(entry)
-        if(entry.fullName != "" && entry.nric != "" && entry.email != "" && entry.password != "" && entry.password2 === entry.password 
-        && entry.dob != "" && entry.gender != "" && entry.address != "") {
+        if(entry.name !== "" && entry.NRIC !== "" && entry.email !== "" && entry.password !== "" && entry.password2 === entry.password 
+        && entry.DOB !== "" && entry.gender !== "" && entry.address !== "") {
             console.log("submitted")
+        }
+        if(entry.password2 !== entry.password)
+        {
+            dispatchItem({ type: "PASSWORD2", value: ""}) 
         }
     }
 
@@ -137,27 +152,27 @@ const CreateAccCard = () => {
             <span className="flex justify-between">
                 <span className="mr-4">
                 <label className="block font-MT text-sm text-gray-700">Full name as per NRIC</label>
-                <input type="text" onChange={handleNameFull} value={entry.fullName} name="" id="" placeholder="" className="w-full px-4 py-2 rounded-lg bg-gray-200 mt-2 border focus:border-blue-500 focus:bg-white focus:outline-none" autofocus autocomplete required />
+                <input type="text" onChange={handleNameFull} value={entry.name} name="" id="" placeholder="" className="w-full px-4 py-2 rounded-lg bg-gray-200 mt-2 border focus:border-blue-500 focus:bg-white focus:outline-none" autoFocus autoComplete required />
                 </span>
             
                 <span className="">
                 <label className="block font-MT text-sm text-gray-700">NRIC</label>
-                <input type="text" onChange={handleNRIC} name="" id="" placeholder="" className="w-full px-4 py-2 rounded-lg bg-gray-200 mt-2 border focus:border-blue-500 focus:bg-white focus:outline-none" autofocus autocomplete required />
+                <input type="text" onChange={handleNRIC} value={entry.NRIC} name="" id="" placeholder="" className="w-full px-4 py-2 rounded-lg bg-gray-200 mt-2 border focus:border-blue-500 focus:bg-white focus:outline-none" autoFocus autoComplete required />
                 </span>
 
             </span>
                
                 <span>
                 <label className="block mt-2 font-MT text-sm text-gray-700">Email</label>
-                    <input type="email" onChange={handleEmail} name="" id="" placeholder="" className="w-full px-4 py-2 rounded-lg bg-gray-200 mt-2 border focus:border-blue-500 focus:bg-white focus:outline-none" autofocus autocomplete required />
+                    <input type="email" onChange={handleEmail} value={entry.email} name="" id="" placeholder="" className="w-full px-4 py-2 rounded-lg bg-gray-200 mt-2 border focus:border-blue-500 focus:bg-white focus:outline-none" autoFocus autoComplete required />
                 </span>
 
                 <span className=''>
                 <label className="block text-sm text-gray-700 mt-4">Password:</label>
                 <span className="flex font-MT">
-                <input type="password" onChange={handlePassword} name="" id="" placeholder="" minlength="6" className="mr-2 w-full px-4 py-2 rounded-lg bg-gray-200 mt-2 border focus:border-blue-500
+                <input type="password" onChange={handlePassword} value={entry.password} name="" id="" placeholder="" minLength="6" className="mr-2 w-full px-4 py-2 rounded-lg bg-gray-200 mt-2 border focus:border-blue-500
                         focus:bg-white focus:outline-none" required />
-                        <input type="password" onChange={handlePassword2} name="" id="" placeholder="Retype password" minlength="6" className=" ml-2 w-full px-4 py-2 rounded-lg bg-gray-200 mt-2 border focus:border-blue-500
+                        <input type="password" onChange={handlePassword2} value={entry.password2} name="" id="" placeholder="Retype password" minLength="6" className=" ml-2 w-full px-4 py-2 rounded-lg bg-gray-200 mt-2 border focus:border-blue-500
                         focus:bg-white focus:outline-none" required />
                         </span>
                 </span>
@@ -166,7 +181,7 @@ const CreateAccCard = () => {
                 <span className="flex justify-between mt-4">
                 
                     <span className="">
-                    <label className="block font-MT text-sm text-gray-700" for="birthdate">Date of birth</label>
+                    <label className="block font-MT text-sm text-gray-700" htmlFor="birthdate">Date of birth</label>
                     <input className="mt-2 p-2 rounded-lg text-gray-700 text-sm" type="date" onChange={handleDOB} id="birthday" name="birthday"/>
                     </span>
 
@@ -175,19 +190,19 @@ const CreateAccCard = () => {
                     <label className="block font-MT text-sm text-gray-700">Gender</label>
 
                     <span className="flex mt-3">   
-                    <label class="flex radio p-2 cursor-pointer">
-                        <input class="my-auto transform scale-125" type="radio" onChange={handleGender} name="gender" value="male" id="male" />
-                            <div class="title px-2">M</div>
+                    <label className="flex radio p-2 cursor-pointer">
+                        <input className="my-auto transform scale-125" type="radio" onChange={handleGender} name="gender" value="male" id="male" />
+                            <div className="title px-2">M</div>
                         </label>
 
-                        <label class="flex radio p-2 cursor-pointer">
-                        <input class="my-auto transform scale-125" type="radio" onChange={handleGender} name="gender" value="female" id="female"  />
-                        <div class="title px-2">F</div>
+                        <label className="flex radio p-2 cursor-pointer">
+                        <input className="my-auto transform scale-125" type="radio" onChange={handleGender} name="gender" value="female" id="female"  />
+                        <div className="title px-2">F</div>
                         </label>
 
-                        <label class="flex radio p-2 cursor-pointer">
-                        <input class="my-auto transform scale-125" type="radio" onChange={handleGender} name="gender" value="others" id="others"  />
-                        <div class="title px-2">Others</div>
+                        <label className="flex radio p-2 cursor-pointer">
+                        <input className="my-auto transform scale-125" type="radio" onChange={handleGender} name="gender" value="others" id="others"  />
+                        <div className="title px-2">Others</div>
                         </label>
                         </span> 
                         </span>
@@ -196,19 +211,23 @@ const CreateAccCard = () => {
 
                 <span className="">
                 <label className="block font-MT text-sm text-gray-700 mt-2">Address <p className="text-xs font-MT text-gray-500">Please include postal code</p></label>
-                <input type="text" onChange={handleAddress} name="address" id="address" placeholder="Street 23, Burlington Road, #01-174, S410897" className="w-full px-4 py-2 rounded-lg bg-gray-200 mt-2 border focus:border-blue-500 focus:bg-white focus:outline-none" autofocus autocomplete required />
+                <input type="text" onChange={handleAddress} value={entry.address} name="address" id="address" placeholder="Street 23, Burlington Road, #01-174, S410897" className="w-full px-4 py-2 rounded-lg bg-gray-200 mt-2 border focus:border-blue-500 focus:bg-white focus:outline-none" autoFocus autoComplete required />
                 </span>
 
+                <span>
+                <label className="block mt-2 font-MT text-sm text-gray-700">Contact Number</label>
+                    <input type="text" onChange={handleContactNumber} value={entry.contactNumber} name="" id="" placeholder="" className="w-full px-4 py-2 rounded-lg bg-gray-200 mt-2 border focus:border-blue-500 focus:bg-white focus:outline-none" autoFocus autoComplete required />
+                </span>
 
                 <span className="flex justify-between mt-4">
                     <span className="mr-4">
                     <label className="block font-MT text-sm text-gray-700">Drug Allergies</label>
-                    <input type="text" onChange={handleAllergies} name="" id="" placeholder="" className="w-full px-4 py-2 rounded-lg bg-gray-200 mt-2 border focus:border-blue-500 focus:bg-white focus:outline-none" autofocus autocomplete required />
+                    <input type="text" onChange={handleAllergies} value={entry.allergies} name="" id="" placeholder="Optional" className="w-full px-4 py-2 rounded-lg bg-gray-200 mt-2 border focus:border-blue-500 focus:bg-white focus:outline-none" autoFocus autoComplete required />
                     </span>
                 
                     <span className="">
                     <label className="block font-MT text-sm text-gray-700">Insurance ID</label>
-                    <input type="text" onChange={handleInsuranceID} name="" id="" placeholder="" className="w-full px-4 py-2 rounded-lg bg-gray-200 mt-2 border focus:border-blue-500 focus:bg-white focus:outline-none" autofocus autocomplete  />
+                    <input type="text" onChange={handleInsuranceID} value={entry.insuranceID} name="" id="" placeholder="Optional" className="w-full px-4 py-2 rounded-lg bg-gray-200 mt-2 border focus:border-blue-500 focus:bg-white focus:outline-none" autoFocus autoComplete  />
                     </span>
 
                 </span>
