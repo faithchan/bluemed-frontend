e3a4a6a69d4aa982903a3c9620efdd5e42af6c2c
faithchan <hannahfaith75@gmail.com>
2022-01-12 14:23:15 +0800

    reverted back to stablisied codes

--- src/components/PastAppCard.js
+++ src/components/PastAppCard.js
@@ -1,42 +1,32 @@
-import React, { useState } from 'react'
-import { useContext} from 'react'
-import { LoginContext, adminContext, userIDContext } from '../global/Context'
+import React, {useState} from 'react'
 import ReceiptModal from './ReceiptModal'
 
-const PastAppCard = ({patient, doctor, type, doctorNotes, attendee, patientNotes, appTime, cost, paid, medicationDelivery, key, showReceipt, setShowReceipt}) => {
-
-  const {userID, setUserID} =  useContext(userIDContext)
-    
+const PastAppCard = () => {
+const[showReceipt, setShowReceipt]=useState(false)
+  
     return (
         <div>
           {showReceipt?<ReceiptModal setShowReceipt={setShowReceipt} showReceipt={showReceipt}/>:""}
             <div className=" rounded-lg bg-white drop-shadow-lg ">
 
-            <div>
-            <div className=" rounded-lg bg-white drop-shadow-lg ">
-
               <div className="flex justify-between p-6">
               <span className="font-MT font-semibold text-xs leading-loose mr-10">
-                <p>Patient: {patient} </p>
-                <p>Attendee: {attendee}</p>
-                <p>Appointment Info: 12.12.21 at 1200H</p>
-                <p>Doctor: {doctor}</p>
-                <p>Type: {type}</p>
-                <p>Doctor Notes: {doctorNotes}</p>
+                <p>Patient: Sarah Lee</p>
+                <p>Appointment Info: 31/12/2021 at 1200H</p>
+                <p>Doctor: Mohit Dhiman</p>
+                <p>Type: Psychotherapy</p>
+                <p>Notes: Weekly Catch Up 1H Session</p>
               </span>
 
               <span className="font-MT font-semibold text-right leading-loose">
               <p className="tracking-wider mb-2">#0001</p>
-              <button className="bg-blue-400 hover:bg-blue-450 text-white font-semibold tracking-widest font-MT py-2 px-4 rounded-full text-xs mx-auto " >View Receipt</button>
+              <button className="bg-blue-400 hover:bg-blue-450 text-white font-semibold tracking-widest font-MT py-2 px-4 rounded-full text-xs mx-auto " onClick={()=>setShowReceipt(true)} >View Receipt</button>
                 <ul className="text-xs flex mt-4">
-                <li><button className="bg-green rounded-full w-4 h-4 mr-2 " onClick={setShowReceipt(true)}/></li>
-                <p className="text-left">{medicationDelivery}</p>
+                <li><button className="bg-green rounded-full w-4 h-4 mr-2 " /></li>
+                <p className="text-left">Completed and delivered</p>
                 </ul>
               </span>
               </div>
- 
-            </div>
-        </div>
  
             </div>
         </div>
