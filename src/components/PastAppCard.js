c469060fdf922e043902d426536f7817f6107df0
faithchan <hannahfaith75@gmail.com>
2022-01-13 18:57:41 +0800

    remove #0001

--- src/components/PastAppCard.js
+++ src/components/PastAppCard.js
@@ -29,7 +29,7 @@ const time = new Date(Date.parse(appInfo)).toLocaleTimeString("en-SG", {hour12: 
               </span>
 
               <span className="font-MT font-semibold text-right leading-loose">
-              <p className="tracking-wider mb-2">#0001</p>
+              
               <button className="bg-blue-400 hover:bg-blue-450 text-white font-semibold tracking-widest font-MT py-2 px-4 rounded-full text-xs mx-auto " onClick={()=>setShowReceipt(true)} >View Receipt</button>
                 <ul className="text-xs flex mt-4">
                 <li><button className="bg-green rounded-full w-4 h-4 mr-2 " /></li>
